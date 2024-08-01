11d5fedd17ee1d18dccba3b669454f667b049962
alliecostner27 <alliecostner3@gmail.com>
2024-08-01 10:58:07 -0400

    finding book attributes

--- main.py
+++ main.py
@@ -2,5 +2,32 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
+current_page = 1
+proceed = True
 
-url = 'https://books.toscrape.com/'
+while(proceed):
+    url = 'https://books.toscrape.com/catalogue/page-'+str(current_page)+'.html'
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text,"html.parser")
+
+    if soup.title.text == "404 Not Found":
+        proceed = False
+    else:
+        all_books = soup.find_all("li", class_= "col-xs-6 col-sm-4 col-md-3 col-lg-3")
+
+        for book in all_books:
+            item = {}
+
+            item['Title'] = book.find("img").attrs["alt"]
+
+            item['Link'] = book.find("a").attrs["href"]
+
+            item['Price'] = book.find("p", class_= "price_color").text[2:]
+
+            item['Stock'] = book.find("p", class_="instock availability")
+
+            print(item['Price'])
+            print(item['Stock'])
+        
+    current_page += 1
+    proceed = False
